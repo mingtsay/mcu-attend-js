def3f2c9fcb9ff6b302c097c22ee02d7793b4ed6
Ming Tsay <mt@moztw.org>
2013-10-02 00:00:00 +0800

    Version 0.9.3

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-/* MCU Attend, Version 0.9.2 */
+/* MCU Attend, Version 0.9.3 */
 /* This script release under LGPL License */
 
 /* jQuery from stackoverflow: 8139794 */
-function loadJQ(onload) {
+var loadJQ = function (onload) {
     var head = document.getElementsByTagName('head')[0];
     var jq = document.createElement('script');
     jq.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js';
@@ -16,9 +16,9 @@ function loadJQ(onload) {
         } else onload();
     };
     head.appendChild(jq);
-}
+};
 
-/* Sound from stackoverflow: 17762763, 7210567 */
+/* Sound from stackoverflow: 17762763 */
 var Sound = (function () {
     var df = document.createDocumentFragment();
     return function Sound(src) {
@@ -32,40 +32,44 @@ var Sound = (function () {
 
 /* CSS from stackoverflow: 524696 */
 var head = document.getElementsByTagName('head')[0];
-var cssxx = 'form td:hover { background: #FFE1FF; } div.regen { display: inline-block; text-align: right; visibility: hidden; } form td:hover div.regen { visibility: visible; }';
+var csstxt = 'form td:hover { background: #FFE1FF; } div.regen { display: inline-block; text-align: right; visibility: hidden; } form td:hover div.regen { visibility: visible; }';
 var mycss = document.createElement("style");
 mycss.type = 'text/css';
 if (mycss.styleSheet) {
-    mycss.styleSheet.cssText = cssxx;
+    mycss.styleSheet.cssText = csstxt;
 } else {
-    mycss.appendChild(document.createTextNode(cssxx));
+    mycss.appendChild(document.createTextNode(csstxt));
 }
 head.appendChild(mycss);
 
-var playSound = function (sid) {
-    var s = new Sound("http://mt.rmstudio.tw/mcu_attend/wave.php?sid=" + sid);
+var playSound = function (sid, name) {
+    var s = new Sound("http://mt.rmstudio.tw/mcu_attend/wave.php?sid=" + sid + "&name=" + name + "&dummy=" + (new Date().getTime()));
 };
 
-var regen = function(sid, name) {
-    var s = new Sound("http://mt.rmstudio.tw/mcu_attend/wave.php?regen&sid=" + sid + "&name=" + name);
+var regen = function(sid) {
+    var s = new Sound("http://mt.rmstudio.tw/mcu_attend/wave.php?regen&sid=" + sid + "&dummy=" + (new Date().getTime()));
 }
+
+var change = function(sid, name) {
+    name = window.prompt("請輸入欲修改之語音內容：", name);
+    var s = new Sound("http://mt.rmstudio.tw/mcu_attend/wave.php?regen&sid=" + sid + "&name=" + name + "&dummy=" + (new Date().getTime()));
+};
 
 var nowid = "";
 
 loadJQ(function () {
     $(function() {
         $("form td").mouseover(function() {
-            var this_id = $(this).find("font")[1].innerHTML;
-            if (nowid != this_id) {
-                playSound(this_id);
-                $("form td").removeClass("xmcuattend");
-                $(this).addClass("xmcuattend");
-                nowid = this_id;
+            var sid = $(this).find("font")[1].innerHTML;
+            var name = $($(this).find("font")[0]).text().substr(sid.length).trim();
+            if (nowid != sid) {
+                playSound(sid, name);
+                nowid = sid;
             }
         }).each(function() {
             var sid = $(this).find("font")[1].innerHTML;
             var name = $($(this).find("font")[0]).text().substr(sid.length).trim();
-            var btn_regen = "<a href=\"javascript:regen('" + sid + "', '" + name + "');\"><img width=\"16px\" height=\"16px\" src=\"http://mt.rmstudio.tw/mcu_attend/images/regen.png\" title=\"重新產生音訊檔案\" alt=\"regen\" /></a>";
+            var btn_regen = "<a href=\"javascript:regen('" + sid + "');\"><img width=\"16px\" height=\"16px\" src=\"http://mt.rmstudio.tw/mcu_attend/images/regen.png\" title=\"重新產生音訊檔案\" alt=\"regen\" /></a>";
             var btn_change = "<a href=\"javascript:change('" + sid + "', '" + name + "');\"><img width=\"16px\" height=\"16px\" src=\"http://mt.rmstudio.tw/mcu_attend/images/change.png\" title=\"修改發音內容\" alt=\"change\" /></a>";
             $(this).prepend($("<div class=\"regen\">" + btn_change + "&nbsp;" + btn_regen + "</div>"));
         });
--- wave.php
+++ wave.php
@@ -24,33 +24,60 @@
 
             $data = $query->fetch();
 
-            if (isset($_GET["regen"]) && isset($_GET["name"])) {
-                $name = $_GET["name"];
-                $wave = getTTSWave($name);
-                $sound = base64_encode($wave);
-
-                if ($data) {
-                    $update = $db->prepare("UPDATE `" . DB_TABLENAME . "` SET `name` = :name, `sound` = :sound WHERE `sid` = :sid");
+            if (isset($_GET["regen"])) {
+                if (isset($_GET["name"])) {
+                    $name = $_GET["name"];
+                } else if ($data) {
+                    $name = $data["name"];
                 } else {
-                    $update = $db->prepare("INSERT INTO `" . DB_TABLENAME . "` (`sid`, `name`, `sound`) VALUES (:sid, :name, :sound);");
+                    $error = array(400, "Please specify name.");
                 }
 
-                $update->bindParam(":sid", $sid);
-                $update->bindParam(":name", $name);
-                $update->bindParam(":sound", $sound);
-                $update->execute();
+                if (!isset($error) && false !== ($wave = getTTSWave($name))) {
+                    $sound = base64_encode($wave);
+
+                    if ($data) {
+                        $update = $db->prepare("UPDATE `" . DB_TABLENAME . "` SET `name` = :name, `sound` = :sound WHERE `sid` = :sid");
+                    } else {
+                        $update = $db->prepare("INSERT INTO `" . DB_TABLENAME . "` (`sid`, `name`, `sound`) VALUES (:sid, :name, :sound);");
+                    }
+
+                    $update->bindParam(":sid", $sid);
+                    $update->bindParam(":name", $name);
+                    $update->bindParam(":sound", $sound);
+                    $update->execute();
+                } else if (!isset($error)) {
+                    $error = array(500, "Error while fetch TTS sound.");
+                }
             } else {
                 if ($data) {
                     $wave = base64_decode($data["sound"]);
                 } else {
-                    $wave = "";
+                    if (isset($_GET["name"])) {
+                        $name = $_GET["name"];
+
+                        if (false !== ($wave = getTTSWave($name))) {
+                            $sound = base64_encode($wave);
+
+                            $update = $db->prepare("INSERT INTO `" . DB_TABLENAME . "` (`sid`, `name`, `sound`) VALUES (:sid, :name, :sound);");
+
+                            $update->bindParam(":sid", $sid);
+                            $update->bindParam(":name", $name);
+                            $update->bindParam(":sound", $sound);
+                            $update->execute();
+                        } else {
+                            $error = array(500, "Error while fetch TTS sound.");
+                        }
+                    } else {
+                        $error = array(404, "Student ID not found.");
+                    }
                 }
             }
         } else {
             $error = array(400, "Bad Student ID.");
         }
     } else {
-        $error = array(400, "Please specify the Student ID and name by HTTP get.");
+        $error = array(400, "Please specify the Student ID by HTTP get.");
     }
 
     if (isset($error)) {
@@ -76,20 +103,20 @@
             $tts_config["output_type"], $tts_config["pitch_level"], $tts_config["pitch_sign"], $tts_config["pitch_scale"]
         ));
 
-        if ($r[0] != "0") {
+        if ((int) $r[0] != 0) {
             return false;
         } else {
             $time = time();
             while (true) {
-                $q = explode("&", $tts->GetConvertStatus(TTS_USERNAME, TTS_PASSWORD, $r[2]));
+                $q = explode("&", $tts->GetConvertStatus(TTS_USERNAME, TTS_PASSWORD, (int) $r[2]));
 
                 if (time() - $time > 90) {
                     return false;
                 } else {
-                    if ($q[0] != "0") {
+                    if ((int) $q[0] != 0) {
                         return false;
                     } else {
-                        if ($q[2] == "2") {
+                        if ((int) $q[2] == 2) {
                             return file_get_contents($q[4]);
                         }
                     }
